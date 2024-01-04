db39e3ecd632befd4a3e2f2c22d120839c4c3c15
unknown <mahmoudabdelaziz93.mz@gmail.com>
2024-01-04 21:32:30 +0200

    fix: logic of editing

--- src/App.js
+++ src/App.js
@@ -3,16 +3,12 @@ import { useEffect, useState } from "react";
 
 const App = () => {
   const [data, setData] = useState([]);
-  const [isEdit, setIsEdit] = useState(false);
   const [addValue, setAddValue] = useState("");
-  const [editValue, setEditValue] = useState("");
-  const [editId, setEditId] = useState(0);
 
   const handleDelete = (id) => {
     const updateDataAfterDelete = data.filter((item) => {
       return item.id !== id;
     });
-    console.log("updateDataAfterDelete: ", updateDataAfterDelete);
     setData(updateDataAfterDelete);
   };
 
@@ -28,29 +24,21 @@ const App = () => {
     setAddValue("");
   };
 
-  const handleEdite = (item) => {
-    console.log(item);
-    console.log("item id is :", item.id);
-    setIsEdit(true);
-    setEditId(item.id);
-    setEditValue(item.todo);
+  const handleEdite = (index) => {
+    const newData = data.slice();
+    newData[index].isEditMode = true;
+    setData(newData);
   };
 
-  const handleChangeOfEdit = (e) => {
-    console.log(e.target.value);
-    setEditValue(e.target.value);
+  const handleChangeOfEdit = (e, index) => {
+    const newData = data.slice();
+    newData[index].todo = e.target.value;
+    setData(newData);
   };
 
-  const handleFinshEdit = () => {
-    const newData = data.map((ele) => {
-      if (ele.id === editId) {
-        ele.todo = editValue;
-        return ele;
-      } else {
-        return ele;
-      }
-    });
-    setIsEdit(false);
+  const handleFinshEdit = (index) => {
+    const newData = data.slice();
+    newData[index].isEditMode = false;
     setData(newData);
   };
 
@@ -59,7 +47,13 @@ const App = () => {
       const response = await fetch("https://dummyjson.com/todos").then((res) =>
         res.json()
       );
-      const newArry = [...data, ...response.todos];
+      const newArry = [
+        ...data,
+        ...response.todos.map((item) => {
+          item.isEditMode = false;
+          return item;
+        }),
+      ];
       setData(newArry);
     };
     getData();
@@ -83,63 +77,33 @@ const App = () => {
             handleAdd(addValue);
           }}
         >
-          {" "}
           Add to todo
         </button>
       </div>
       <div className="border m-10">
         <ul className="list-[upper-roman] inline-block mt-3 w-full">
-          {" "}
           <div className="m-2">
-            {data.map((item) =>
-              isEdit ? (
-                editId === item.id ? (
-                  <div
-                    key={item.id}
-                    className="flex flex-row w-auto justify-between"
-                  >
-                    <input
-                      type="text"
-                      value={editValue}
-                      className="border w-max rounded-2xl"
-                      onChange={(e) => handleChangeOfEdit(e)}
-                    />
-                    <div className="w-auto mr-20">
-                      <button
-                        onClick={() => handleFinshEdit(item)}
-                        className="btn btn-primary m-2"
-                      >
-                        {" "}
-                        OK
-                      </button>
-                    </div>
+            {data.map((item, index) =>
+              item.isEditMode ? (
+                <div
+                  key={item.id}
+                  className="flex flex-row w-auto justify-between"
+                >
+                  <input
+                    type="text"
+                    value={item.todo}
+                    className="border w-max rounded-2xl"
+                    onChange={(e) => handleChangeOfEdit(e, index)}
+                  />
+                  <div className="w-auto mr-20">
+                    <button
+                      onClick={() => handleFinshEdit(index)}
+                      className="btn btn-primary m-2"
+                    >
+                      OK
+                    </button>
                   </div>
-                ) : (
-                  <li
-                    className="flex flex-row w-auto justify-between"
-                    key={item.id}
-                  >
-                    {item.todo}
-                    <div className="w-auto mr-20">
-                      <button
-                        className="btn btn-primary m-2"
-                        onClick={() => {
-                          handleDelete(item.id);
-                        }}
-                      >
-                        {" "}
-                        DELETE
-                      </button>
-                      <button
-                        onClick={() => handleEdite(item)}
-                        className="btn btn-primary m-2"
-                      >
-                        {" "}
-                        EDIT
-                      </button>
-                    </div>
-                  </li>
-                )
+                </div>
               ) : (
                 <li
                   className="flex flex-row w-auto justify-between"
@@ -153,14 +117,12 @@ const App = () => {
                         handleDelete(item.id);
                       }}
                     >
-                      {" "}
                       DELETE
                     </button>
                     <button
-                      onClick={() => handleEdite(item)}
+                      onClick={() => handleEdite(index)}
                       className="btn btn-primary m-2"
                     >
-                      {" "}
                       EDIT
                     </button>
                   </div>
