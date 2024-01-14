065c5a65f5bc4067307b1c9ff42f78130d6849a2
unknown <mahmoudabdelaziz93.mz@gmail.com>
2024-01-14 20:46:31 +0200

    fix: resolved  testing code & Generation funtion

--- src/App.js
+++ src/App.js
@@ -1,3 +1,2 @@
 import { HomePage } from "./containers/HomePage";
-const App = () => <HomePage />;
-export default App;
+export const App = () => <HomePage />;
--- src/utils/getRandomNumber/index.js
+++ src/utils/getRandomNumber/index.js
@@ -1,8 +1,25 @@
 export const getRandomNumber = (digit) => {
   let id;
   do {
-    const randomNumber = Math.random() * Math.pow(10, digit);
-    id = Math.floor(randomNumber);
-  } while (id.toString()[0] === "0");
-  return id;
+    const randomNumber = Math.floor(Math.random() * Math.pow(10, digit));
+    id = randomNumber.toString();
+  } while (id.length !== digit);
+  return parseInt(id);
 };
+// const generateRandomNumbersArray = (digit, count) => {
+//   const randomNumbersArray = [];
+
+//   for (let i = 0; i < count; i++) {
+//     const randomNumber = getRandomNumber(digit);
+//     randomNumbersArray.push(randomNumber);
+//   }
+
+//   return randomNumbersArray;
+// };
+
+// // Example usage
+// const digit = 6; // Set the desired number of digits
+// const count = 1000; // Set the desired count of random numbers
+
+// const arrayOfRandomNumbers = generateRandomNumbersArray(digit, count);
+// console.log(arrayOfRandomNumbers);
