087155590c76c173f60184915e20eecbe7ecba5f
unknown <mahmoudabdelaziz93.mz@gmail.com>
2024-01-14 13:06:52 +0200

    fix: remove additional fragment &renamed the edit button

--- src/components/todoItem/index.js
+++ src/components/todoItem/index.js
@@ -56,7 +56,7 @@ export const TodoItem = ({
               onClick={onFinishEditClick}
               className="bg-blue-500 text-white py-2 px-4 rounded m-3"
             >
-              Eidt
+              Edit
             </Button>
           </div>
         </li>
--- src/components/todoItemList/index.js
+++ src/components/todoItemList/index.js
@@ -6,26 +6,24 @@ export const TodoItemList = ({
   handleDelete,
 }) => {
   return (
-    <>
-      <div className="border m-10">
-        <ul className="inline-block mt-3 w-full">
-          <div className="m-2">
-            {data.map((item) => (
-              <TodoItem
-                editMode={item.isEditMode}
-                key={item.id}
-                todo={item.todo}
-                onEditClick={(e) => handleEditChange(e, item)}
-                onFinishEditClick={() => toggleEditMode(item)}
-                onDeleteClick={() => {
-                  handleDelete(item.id);
-                }}
-                onClickEdit={() => toggleEditMode(item)}
-              />
-            ))}
-          </div>
-        </ul>
-      </div>
-    </>
+    <div className="border m-10">
+      <ul className="inline-block mt-3 w-full">
+        <div className="m-2">
+          {data.map((item) => (
+            <TodoItem
+              editMode={item.isEditMode}
+              key={item.id}
+              todo={item.todo}
+              onEditClick={(e) => handleEditChange(e, item)}
+              onFinishEditClick={() => toggleEditMode(item)}
+              onDeleteClick={() => {
+                handleDelete(item.id);
+              }}
+              onClickEdit={() => toggleEditMode(item)}
+            />
+          ))}
+        </div>
+      </ul>
+    </div>
   );
 };
